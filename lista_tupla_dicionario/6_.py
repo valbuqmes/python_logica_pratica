b499dbcdb11d77affe8e8d6ab323eff7babfa8e0
vinicius <v.albuqmes@gmail.com>
2026-04-14 21:36:35 -0300

    adicionando esse exercicio

--- lista_tupla_dicionario/6_.py
+++ lista_tupla_dicionario/6_.py
@@ -4,3 +4,28 @@
 #• Para cada atleta, leia os cinco valores de salto e armazene em uma lista.
 #• Exiba o nome, os saltos e a média calculada.
 #• O programa deve aceitar múltiplos atletas até que nome vazio seja informado.
+
+
+atletas = []
+
+while True:
+    a = {}
+    
+  
+    a['nome'] = input("Digite o nome do atleta: ").strip()
+    if not a['nome']:#como string é valor verdadeiro e string vazia é falso, se for falso nem entra no looping mas agora, com o not, foi negado então se a string estiver vazia executa esse bloco e finaliza o codigo
+             break
+     
+    a['primeiro_salto'] = float(input("Digite a distancia do primeiro salto: "))
+    a['segundo_salto']  = float(input("Digite a distancia do segundo salto: "))
+    a['terceiro_salto'] = float(input("Digite a distancia do terceiro salto: "))
+    a['quarto_salto'] = float(input("Digite a distancia do quarto salto: "))
+    a['quinto_salto'] = float(input("Digite a distancia do quinto salto: "))
+    a['media'] = (a['primeiro_salto'] + a['segundo_salto'] + a['terceiro_salto'] + a['quarto_salto'] + a['quinto_salto']) / 5
+
+    atletas.append(a)
+
+print("Programa encerrado!")
+print(atletas)
+
+    
